d30399da376e8e4d4340ef11e3e06c60207988fc
daijun <junsmooth@163.com>
2013-03-15 16:24:56 +0800

    Signed-off-by: daijun <junsmooth@163.com>

---
+++ base/src/main/java/org/bgrimm/persistence/UserDao.java
@@ -0,0 +1,12 @@
+package org.bgrimm.persistence;
+
+import java.awt.Menu;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+public class UserDao {
+	
+}
