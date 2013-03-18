07447c41bd7f82bfb8f186714cf7dd2f92af19a3
daijun <junsmooth@163.com>
2013-03-18 15:34:01 +0800

    Signed-off-by: daijun <junsmooth@163.com>

--- base/src/main/java/org/bgrimm/persistence/MenuDao.java
+++ base/src/main/java/org/bgrimm/persistence/MenuDao.java
@@ -5,8 +5,10 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
 import org.bgrimm.domain.Menu;
@@ -15,31 +17,22 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public class MenuDao {
-	private String menuQuery = "SELECT m FROM MENU m where m.parent.id = ?1 ";
+	// private String menuQuery =
+	// "SELECT m FROM MENU m where m.parent.id = ?1 ";
 
 	@PersistenceContext
 	private EntityManager em;
 
 	@Transactional
 	public List<Menu> getMenusByParentId(int pid) {
-		this.insert();
 		CriteriaBuilder cb = em.getCriteriaBuilder();
-		CriteriaQuery q = cb.createQuery(Menu.class);
-		Root r=q.from(Menu.class);
-		
-		
-
-		Query query = em.createQuery(menuQuery);
-		query.setParameter(1, pid);
-		List<Menu> menus = query.getResultList();
-		return menus;
-	}
-
-	public void insert() {
-		Menu m = new Menu();
-		m.setLeaf(false);
-		m.setText("系统管理");
-		em.persist(m);
+		CriteriaQuery<Menu> cq = cb.createQuery(Menu.class);
+		Root<Menu> r = cq.from(Menu.class);
+		cq.select(r);
+		cq.where(cb.equal(r.get("parent"), null));
+		TypedQuery<Menu> tq = em.createQuery(cq);
+		List<Menu> result = tq.getResultList();
+		return result;
 	}
 
 }
