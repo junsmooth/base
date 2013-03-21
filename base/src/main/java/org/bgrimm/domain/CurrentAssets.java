2caa2a59e140294970c586908549b9b8829b8181
daijun <junsmooth@163.com>
2013-03-21 15:49:38 +0800

    add dojo support

---
+++ base/src/main/java/org/bgrimm/domain/CurrentAssets.java
@@ -0,0 +1,14 @@
+package org.bgrimm.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+//流动资产明细(现金/银行存款等)
+@Entity
+public class CurrentAssets {
+	@Id
+	@GeneratedValue
+	private long id;
+
+}
