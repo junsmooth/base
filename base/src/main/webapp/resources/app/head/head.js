d022c2d74a9b62dd10cb26b397598b75393fc2d8
daijun <junsmooth@163.com>
2013-03-25 16:52:27 +0800

    logo test

--- base/src/main/webapp/resources/app/head/head.js
+++ base/src/main/webapp/resources/app/head/head.js
@@ -2,14 +2,7 @@ require([ "dojo/_base/declare", "dijit/_WidgetBase", "dijit/_TemplatedMixin",
 		"dojo/text!app/head/head.html" ], function(declare, _WidgetBase,
 		_TemplatedMixin, template) {
 	declare("FancyCounter", [ _WidgetBase, _TemplatedMixin ], {
-		// counter
 		_i : 0,
-
-		// templateString: "<div>" +
-		// "<button data-dojo-attach-event='onclick: increment'>press
-		// me</button>" +
-		// "&nbsp; count: <span data-dojo-attach-point='counter'>0</span>" +
-		// "</div>",
 		templateString : template,
 		increment : function() {
 			this.counter.innerHTML = ++this._i;
