085f26f434d5d451a3f2bc39c117d71386fa9a57
daijun <junsmooth@163.com>
2013-03-28 15:12:39 +0800

    Dojo Base Usage Completed

--- base/src/main/java/org/bgrimm/controller/MenuController.java
+++ base/src/main/java/org/bgrimm/controller/MenuController.java
@@ -1,14 +1,13 @@
 package org.bgrimm.controller;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
+import org.bgrimm.domain.Menu;
 import org.bgrimm.service.MenuService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,18 +25,7 @@ public class MenuController {
 
 		if ("root".equals(node)) {
 			return service.getMainMenus();
-			// Map m1 = new HashMap();
-			// m1.put("text", "User Mangement");
-			// m1.put("leaf", false);
-			// m1.put("id", 1);
-			// list.add(m1);
-			// Map m2 = new HashMap();
-			// m2.put("text", "Security Mangement");
-			// m2.put("leaf", false);
-			// m2.put("id", 2);
-			// list.add(m2);
 		} else {
-
 			try {
 				int inde = Integer.parseInt(node);
 				return service.getSubMenus(inde);
@@ -45,37 +33,26 @@ public class MenuController {
 				e.printStackTrace();
 				return new ArrayList();
 			}
-
 		}
-		// else if ("1".equals(node)) {
-		// Map m1 = new HashMap();
-		// m1.put("text", "Sub1");
-		// m1.put("leaf", true);
-		// m1.put("id", 3);
-		// m1.put("controller", "TestController");
-		// list.add(m1);
-		// Map m2 = new HashMap();
-		// m2.put("text", "Sub 2");
-		// m2.put("leaf", true);
-		// m2.put("id", 4);
-		// m2.put("controller", "TestController");
-		// list.add(m2);
-		// } else {
-		// Map m1 = new HashMap();
-		// m1.put("text", "User Mangement");
-		// m1.put("leaf", false);
-		// m1.put("id", 5);
-		// list.add(m1);
-		// Map m2 = new HashMap();
-		// m2.put("text", "Security Mangement");
-		// m2.put("leaf", false);
-		// m2.put("id", 6);
-		// list.add(m2);
-		// }
-
-		// Object principal = SecurityContextHolder.getContext()
-		// .getAuthentication();
-		// System.out.println("-------" + principal);
-		// return list;
+	}
+	@RequestMapping
+	public @ResponseBody List<Menu> menu(@RequestParam(value="id", required=false) String id,@RequestParam(value="parent", required=false) String parent ){
+		List<Menu> list = new ArrayList<Menu>();
+		//virtual root
+		if("root".equals(id)){
+			Menu m=new Menu();
+			m.setId(0);
+			m.setText("Root");
+			m.setLeaf(false);
+			list.add(m);
+//			return service.getMainMenus();
+			return list;
+		}
+		if("0".equals(parent)){
+			return service.getMainMenus();
+		}else{
+			return service.getSubMenus(Integer.parseInt(parent));
+		}
+		
 	}
 }
