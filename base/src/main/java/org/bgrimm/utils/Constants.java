286fad97d7c0155a194f0aa72834cfdbe1d38177
daijun <junsmooth@163.com>
2013-06-25 13:49:26 +0800

    Add:后台数据同步服务

--- base/src/main/java/org/bgrimm/utils/Constants.java
+++ base/src/main/java/org/bgrimm/utils/Constants.java
@@ -2,21 +2,21 @@ package org.bgrimm.utils;
 
 public class Constants {
 	public static final String AUTH_SYSTEM = "ROLE_SYSTEM";
-	public static final String AUTH_DEFAULT_VIEW="ROLE_ROOT_VIEW";
-	public static final String AUTH_DENIED_VIEW="_DENIED_VIEW_";
-	public static final String ROOT_VIEW_URL="menu/left";
-	
-	
-	public static final String JCD_JRX="JRX";
-	public static final String JCD_BMWY="BMWY";
-	public static final String JCD_GTGC="GTGC";
-	public static final String JCD_GTCD="GTCD";
-	public static final String JCD_WATERLEVEL="KSW";
-	public static final String JCD_RAINFALL="JYL";
-	public static final String JCD_FREEBOARD="AQCG";
-	public static final String JCD_DAMCREST="BDGC";
-	public static final String JCD_SEEPAGE="SLL";
-	public static final String JCD_INNERDISP="NBWY";
+	public static final String AUTH_DEFAULT_VIEW = "ROLE_ROOT_VIEW";
+	public static final String AUTH_DENIED_VIEW = "_DENIED_VIEW_";
+	public static final String ROOT_VIEW_URL = "menu/left";
+
+	public static final String JCD_JRX = "JRX";
+	public static final String JCD_BMWY = "BMWY";
+	public static final String JCD_GTGC = "GTGC";
+	public static final String JCD_GTCD = "GTCD";
+	public static final String JCD_WATERLEVEL = "KSW";
+	public static final String JCD_RAINFALL = "JYL";
+	public static final String JCD_FREEBOARD = "AQCG";
+	public static final String JCD_DAMCREST = "BDGC";
+	public static final String JCD_SEEPAGE = "SLL";
+	public static final String JCD_INNERDISP = "NBWY";
 	public static final String JCD_SP = "SP";
-	
+	public static String MIGRATION_PACKAGE = "org.bgrimm.domain.bgrimm.monitor.datamigration";
+
 }
